ed20cc4d09213d9eb646f48a9b2ebcf6dff2da7f
gheysiell <gheysiellfarukzinn@hotmail.com>
2022-10-09 01:14:23 -0400

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,152 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { 
+    View,
+    KeyboardAvoidingView,
+    Image,
+    TextInput,
+    TouchableOpacity,
+    Text,
+    StyleSheet,
+    Animated,
+    Keyboard,
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [offset] = useState(new Animated.ValueXY({x: 0, y: 95}))
+    const [opacity] = useState(new Animated.Value(0))
+    const [logo] = useState(new Animated.ValueXY({x: 130, y: 155}))
+
+    useEffect(() => {
+        keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', keyboardDidShow)
+        keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', keyboardDidHide)
+
+        Animated.parallel([
+            Animated.spring(offset.y, {
+                toValue: 0,
+                speed: 4,
+                bounciness: 20,
+            }),
+            Animated.timing(opacity, {
+                toValue: 1,
+                duration: 200,
+            })
+        ]).start()
+    }, [])
+
+    function keyboardDidShow() {
+        Animated.parallel([
+            Animated.timing(logo.x, {
+                toValue: 55,
+                duration: 100,
+            }),
+            Animated.timing(logo.y, {
+                toValue: 65,
+                duration: 100,
+            }),
+        ]).start()
+    }
+
+    function keyboardDidHide() {
+        Animated.parallel([
+            Animated.timing(logo.x, {
+                toValue: 130,
+                duration: 100,
+            }),
+            Animated.timing(logo.y, {
+                toValue: 155,
+                duration: 100,
+            }),
+        ]).start()
+    }
+
+    return (
+        <KeyboardAvoidingView style={styles.background}>
+            <View style={styles.containerLogo}>
+                <Animated.Image
+                    style={{
+                        width: logo.x,
+                        height: logo.y,
+                    }}
+                    source={require('./src/assets/logo.png')}
+                />
+            </View>
+            <Animated.View
+                style={[
+                    styles.container,
+                    {
+                        opacity: opacity,
+                        transform: [
+                            { translateY: offset.y }
+                        ]
+                    }
+                ]}
+            >
+                <TextInput
+                    style={styles.input}
+                    placeholder='e-mail'
+                    autoCorrect={false}
+                    onChangeText={() => {}}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='e-mail'
+                    autoCorrect={false}
+                    onChangeText={() => {}}
+                />
+                <TouchableOpacity style={styles.btnSubmit}>
+                    <Text style={styles.submitText}>Acessar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.btnRegister}>
+                    <Text style={styles.registerText}>Criar conta gratuita</Text>
+                </TouchableOpacity>
+            </Animated.View>
+        </KeyboardAvoidingView>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    background: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#191919',        
+    },
+    containerLogo: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '90%',
+        paddingBottom: 50,
+    },
+    input: {
+        backgroundColor: '#fff',
+        width: '90%',
+        marginBottom: 15,
+        color: '#222',
+        fontSize: 17,
+        borderRadius: 7,
+        padding: 10,
+    },
+    btnSubmit: {
+        backgroundColor: '#35AAFF',
+        width: '90%',
+        height: 45,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 7,
+    },
+    submitText: {
+        color: '#fff',
+        fontSize: 18,
+    },
+    btnRegister: {
+        marginTop: 10,        
+    },
+    registerText: {
+        color: "#fff",
+    },
+})
---
+++ README.md
@@ -0,0 +1,5 @@
+# animated_login
+
+<div> <h2> Mobile App with animated login interface made with React Native with expo </h2> </div>
+<div> <h5> ° For instaling dependences, npm install </h5> </div>
+<div> <h5> ° For init project go into the same folder, and run npm start </h5> </div>
